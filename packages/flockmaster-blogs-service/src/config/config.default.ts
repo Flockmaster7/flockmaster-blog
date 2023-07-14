0ece715ac55d5c0d1858256d811a2870d26b448b
Flockmaster <2417132624@qq.com>
2023-07-14 21:23:27 +0800

    perf: 后端环境自动配置

--- packages/flockmaster-blogs-service/src/config/config.default.ts
+++ packages/flockmaster-blogs-service/src/config/config.default.ts
@@ -4,4 +4,5 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 // module.exports = process.env;
+
 export default process.env;
--- packages/flockmaster-blogs-service/src/db/index.ts
+++ packages/flockmaster-blogs-service/src/db/index.ts
@@ -10,30 +10,50 @@ const {
 	MYSQL_SERVICE_USER,
 	MYSQL_SERVICE_PWD,
 	MYSQL_SERVICE_DB,
-	MYSQL_SERVICE_HOST
+	MYSQL_SERVICE_HOST,
+	NODE_ENV
 } = processEnv;
 
 // 本地数据库
-const sequelize = new Sequelize(
-	MYSQL_DB as string,
-	MYSQL_USER as string,
-	MYSQL_PWD,
-	{
-		host: MYSQL_HOST,
-		dialect: 'mysql',
-		timezone: '+8:00', //设置为东八区
-		models: [
-			`${path.resolve(__dirname, '../model')}/*.ts`,
-			`${path.resolve(__dirname, '../model')}/*.js`
-		], // 数据库模板存放地址
-		// 配置日期格式
-		dialectOptions: {
-			dateStrings: true,
-			typeCast: true,
-			useUTC: false
-		}
-	}
-);
+const sequelize =
+	NODE_ENV === 'development'
+		? // 本地数据库
+		  new Sequelize(MYSQL_DB as string, MYSQL_USER as string, MYSQL_PWD, {
+				host: MYSQL_HOST,
+				dialect: 'mysql',
+				timezone: '+8:00', //设置为东八区
+				models: [
+					`${path.resolve(__dirname, '../model')}/*.ts`,
+					`${path.resolve(__dirname, '../model')}/*.js`
+				], // 数据库模板存放地址
+				// 配置日期格式
+				dialectOptions: {
+					dateStrings: true,
+					typeCast: true,
+					useUTC: false
+				}
+		  })
+		: //   服务器数据库
+		  new Sequelize(
+				MYSQL_SERVICE_DB as string,
+				MYSQL_SERVICE_USER as string,
+				MYSQL_SERVICE_PWD,
+				{
+					host: MYSQL_SERVICE_HOST,
+					dialect: 'mysql',
+					timezone: '+8:00', //设置为东八区
+					models: [
+						`${path.resolve(__dirname, '../model')}/*.ts`,
+						`${path.resolve(__dirname, '../model')}/*.js`
+					], // 数据库模板存放地址
+					// 配置日期格式
+					dialectOptions: {
+						dateStrings: true,
+						typeCast: true,
+						useUTC: false
+					}
+				}
+		  );
 
 // 服务器数据库
 // const sequelize = new Sequelize(
